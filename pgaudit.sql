5b394e61a210ffee35e530fb3c99df73bdc218be
PonuryŻniwiarz <alicja.kucharczyk@linuxpolska.pl>
2016-11-24 16:52:39 +0100

    adding truncate trigger

--- pgaudit.sql
+++ pgaudit.sql
@@ -41,23 +41,31 @@ CREATE OR REPLACE FUNCTION audit.config_insert()
 
 /* CREATE TRIGGERS */
 
-    l_query_create := 'CREATE TRIGGER trigger_' || NEW.table_name || '_audit_update
+    l_query_create := '
+              CREATE TRIGGER trigger_' || NEW.table_name || '_audit_update
               BEFORE UPDATE
-              ON ' || NEW.schema_name || '.' || NEW.table_name ||
-                      ' FOR EACH ROW
-                      EXECUTE PROCEDURE audit.update();
+              ON ' || NEW.schema_name || '.' || NEW.table_name || '
+              FOR EACH ROW
+              EXECUTE PROCEDURE audit.update();
 
-             CREATE TRIGGER trigger_' || NEW.table_name || '_audit_insert
-             AFTER INSERT
-             ON ' || NEW.schema_name || '.' || NEW.table_name || '
-             FOR EACH ROW
-             EXECUTE PROCEDURE audit.insert();
+              CREATE TRIGGER trigger_' || NEW.table_name || '_audit_insert
+              AFTER INSERT
+              ON ' || NEW.schema_name || '.' || NEW.table_name || '
+              FOR EACH ROW
+              EXECUTE PROCEDURE audit.insert();
 
-            CREATE TRIGGER trigger_' || NEW.table_name || '_audit_delete
-             BEFORE DELETE
-             ON ' || NEW.schema_name || '.' || NEW.table_name || '
-             FOR EACH ROW
-             EXECUTE PROCEDURE audit.delete();';
+              CREATE TRIGGER trigger_' || NEW.table_name || '_audit_delete
+              BEFORE DELETE
+              ON ' || NEW.schema_name || '.' || NEW.table_name || '
+              FOR EACH ROW
+              EXECUTE PROCEDURE audit.delete();
+
+              CREATE TRIGGER trigger_' || NEW.table_name || '_audit_truncate
+              BEFORE TRUNCATE
+              ON ' || NEW.schema_name || '.' || NEW.table_name || '
+              FOR EACH STATEMENT
+              EXECUTE PROCEDURE audit.truncate();
+              ';
 
     EXECUTE l_query_create;
     RETURN NULL;
@@ -86,7 +94,11 @@ CREATE OR REPLACE FUNCTION audit.config_update()
                         ' DISABLE TRIGGER trigger_' || OLD.table_name || '_audit_insert;
 
                       ALTER TABLE ' || OLD.schema_name || '.' || OLD.table_name ||
-                        ' DISABLE TRIGGER trigger_' || OLD.table_name || '_audit_delete;';
+                        ' DISABLE TRIGGER trigger_' || OLD.table_name || '_audit_delete;
+
+                      ALTER TABLE ' || OLD.schema_name || '.' || OLD.table_name ||
+                        ' DISABLE TRIGGER trigger_' || OLD.table_name || '_audit_truncate;
+                      ';
 
       EXECUTE l_query_create;
       RETURN NEW;
@@ -99,7 +111,11 @@ CREATE OR REPLACE FUNCTION audit.config_update()
                         ' ENABLE TRIGGER trigger_' || OLD.table_name || '_audit_insert;
 
                       ALTER TABLE ' || OLD.schema_name || '.' || OLD.table_name ||
-                        ' ENABLE TRIGGER trigger_' || OLD.table_name || '_audit_delete;';
+                        ' ENABLE TRIGGER trigger_' || OLD.table_name || '_audit_delete;
+
+                      ALTER TABLE ' || OLD.schema_name || '.' || OLD.table_name ||
+                        ' ENABLE TRIGGER trigger_' || OLD.table_name || '_audit_truncate;
+                      ';
 
       EXECUTE l_query_create;
       RETURN NEW;
@@ -121,14 +137,18 @@ CREATE OR REPLACE FUNCTION audit.config_delete()
 
 /* DELETE TRIGGERS */
 
-    l_query_create := 'DROP TRIGGER trigger_' || OLD.table_name || '_audit_update
+    l_query_create := '
+            DROP TRIGGER trigger_' || OLD.table_name || '_audit_update
               ON ' || OLD.schema_name || '.' || OLD.table_name || ';
 
-             DROP TRIGGER trigger_' || OLD.table_name || '_audit_insert
-             ON ' || OLD.schema_name || '.' || OLD.table_name || ';
+            DROP TRIGGER trigger_' || OLD.table_name || '_audit_insert
+              ON ' || OLD.schema_name || '.' || OLD.table_name || ';
 
             DROP TRIGGER trigger_' || OLD.table_name || '_audit_delete
-             ON ' || OLD.schema_name || '.' || OLD.table_name;
+              ON ' || OLD.schema_name || '.' || OLD.table_name || ';
+
+            DROP TRIGGER trigger_' || OLD.table_name || '_audit_truncate
+              ON ' || OLD.schema_name || '.' || OLD.table_name;
 
     EXECUTE l_query_create;
     RETURN NULL;
@@ -184,7 +204,7 @@ CREATE OR REPLACE FUNCTION audit.update()
     l_table_name   TEXT := quote_ident(TG_TABLE_SCHEMA || '_' || TG_TABLE_NAME || l_table_suffix);
 
   BEGIN
-/* ADD ROW ONLY IF SOMETHING CHANGED */
+  /* ADD ROW ONLY IF SOMETHING CHANGED */
 
     IF md5(NEW :: TEXT) = md5(OLD :: TEXT)
     THEN
@@ -257,6 +277,42 @@ CREATE OR REPLACE FUNCTION audit.delete()
 LANGUAGE plpgsql VOLATILE SECURITY DEFINER
 COST 100;
 ALTER FUNCTION audit.delete() SET search_path=audit, PUBLIC;
+
+CREATE OR REPLACE FUNCTION audit.truncate()
+  RETURNS trigger AS
+$BODY$
+  DECLARE
+    l_table_suffix TEXT := to_char(NOW(), '_YYYY_MM');
+    l_query        TEXT;
+    l_table_name   TEXT := quote_ident(TG_TABLE_SCHEMA || '_' || TG_TABLE_NAME || l_table_suffix);
+    l_table_name_original TEXT := TG_TABLE_NAME;
+  BEGIN
+    l_query:= format('INSERT INTO %I SELECT $1, $2, $3, * FROM %I', l_table_name, l_table_name_original);
+
+    EXECUTE l_query
+    USING CURRENT_TIMESTAMP, SESSION_USER, TG_OP;
+
+    RETURN NULL;
+
+    EXCEPTION WHEN UNDEFINED_TABLE
+    THEN
+      PERFORM audit._partitions_month_table_creator(i_schema_name := TG_TABLE_SCHEMA, i_table_name := TG_TABLE_NAME,
+                                                      i_table_suffix := l_table_suffix, i_stamp_column := NOW());
+
+      EXECUTE l_query
+      USING CURRENT_TIMESTAMP, SESSION_USER, TG_OP;
+      RETURN NULL;
+
+    WHEN OTHERS
+      THEN
+        RAISE NOTICE 'ERROR: INTERNAL ERROR IN AUDIT FUNCTION audit.truncate()';
+        RETURN NULL;
+
+  END;
+  $BODY$
+  LANGUAGE plpgsql VOLATILE SECURITY DEFINER
+  COST 100;
+ALTER FUNCTION audit.truncate() SET search_path=audit, public;
 
 CREATE TABLE audit.abstract
 (
